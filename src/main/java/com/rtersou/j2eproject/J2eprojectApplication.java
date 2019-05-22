b84454434cf366365cff180b8966dd55bfa341c5
RobinTersou <tersou.robin@gmail.com>
2019-05-22 13:47:31 +0200

    debug

--- src/main/java/com/rtersou/j2eproject/J2eprojectApplication.java
+++ src/main/java/com/rtersou/j2eproject/J2eprojectApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class J2eprojectApplication extends SpringBootServletInitializer {
+public class J2eprojectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(J2eprojectApplication.class, args);

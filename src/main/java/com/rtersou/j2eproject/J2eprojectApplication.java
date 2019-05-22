1095247b68b352ba4b33d28c3e91be76cd81e1b7
RobinTersou <tersou.robin@gmail.com>
2019-05-22 14:19:22 +0200

    tomcat

--- src/main/java/com/rtersou/j2eproject/J2eprojectApplication.java
+++ src/main/java/com/rtersou/j2eproject/J2eprojectApplication.java
@@ -4,7 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class J2eprojectApplication {
+public class J2eprojectApplication extends SpringBootServletInitializer  {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(J2eprojectApplication.class, args);

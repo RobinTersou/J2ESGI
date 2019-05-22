9ffbfee3f546c1e2b7a41012f3942a446e933d0d
RobinTersou <tersou.robin@gmail.com>
2019-05-22 14:36:56 +0200

    tomcat

--- src/main/java/com/rtersou/j2eproject/J2eprojectApplication.java
+++ src/main/java/com/rtersou/j2eproject/J2eprojectApplication.java
@@ -2,6 +2,8 @@ package com.rtersou.j2eproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
 public class J2eprojectApplication extends SpringBootServletInitializer  {

60faf21c506f38d50f53655fef3b0087a9e29a33
RobinTersou <tersou.robin@gmail.com>
2019-05-28 11:02:29 +0200

    test2

--- src/main/java/com/rtersou/j2eproject/Exceptions/PartyNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.rtersou.j2eproject.Exceptions;
-
-public class PartyNotFoundException extends RuntimeException {
-    public PartyNotFoundException(Long id) {
-        super("Could not find Party " + id);
-    }
-}
--- src/main/java/com/rtersou/j2eproject/models/user/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.rtersou.j2eproject.models.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}

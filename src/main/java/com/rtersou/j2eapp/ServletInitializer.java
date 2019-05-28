0d4e413185a678ce86effe729417c589fc4bb067
RobinTersou <tersou.robin@gmail.com>
2019-05-28 12:12:55 +0200

    test

--- src/main/java/com/rtersou/j2eapp/ServletInitializer.java
+++ src/main/java/com/rtersou/j2eapp/ServletInitializer.java
@@ -2,7 +2,9 @@ package com.rtersou.j2eapp;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan(basePackages= {"com.rtersou.j2eapp.controllers"})
 public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override

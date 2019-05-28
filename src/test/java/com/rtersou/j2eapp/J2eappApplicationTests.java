67fb894abfada40d08924d595dd3e9e73e9dc03a
RobinTersou <tersou.robin@gmail.com>
2019-05-28 11:09:52 +0200

    t

--- src/test/java/com/rtersou/j2eapp/J2eappApplicationTests.java
+++ src/test/java/com/rtersou/j2eapp/J2eappApplicationTests.java
@@ -8,9 +8,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class J2eappApplicationTests {
-/*
+
 	@Test
 	public void contextLoads() {
 	}
-*/
+
 }
